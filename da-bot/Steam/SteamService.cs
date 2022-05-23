f71673790ae877e447454c674028a31ebe57bd11
diggforbeer <diggforbeer@gmail.com>
2022-05-23 09:59:15 -0500

    update just incase there happens to be multiple added due to async threading ( shouldnt but good saftey)

--- da-bot/Steam/SteamService.cs
+++ da-bot/Steam/SteamService.cs
@@ -5,21 +5,33 @@ namespace da_bot.Steam
 {
     public class SteamService
     {
-        private readonly string apiKey;
+        private readonly string _apiKey;
+        private static List<Player> _cachedPlayers = new List<Player>();
 
         public SteamService(string apiKey)
         {
-            this.apiKey = apiKey;
+            _apiKey = apiKey;
         }
 
         public async Task<Player?> GetPlayerInfo(string playerId)
         {
+
+            var cachedPlayer = _cachedPlayers.FirstOrDefault(x => x.Steamid == playerId);
+            if (cachedPlayer != null)
+                return cachedPlayer;
+
+
             using (var client = new HttpClient())
             {
-                var response = await client.GetAsync($"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key={apiKey}&steamids={playerId}");
+                var response = await client.GetAsync($"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key={_apiKey}&steamids={playerId}");
                 var message = await response.Content.ReadAsStringAsync();
                 var playerInfo = JsonConvert.DeserializeObject<PlayerResponse>(message);
-                return playerInfo.Response?.Players?.Player?.FirstOrDefault();
+                var currentPlayer = playerInfo.Response?.Players?.Player?.FirstOrDefault();
+                
+                //Add to static if its not null so the cache can retreive it.
+                if ( currentPlayer!= null)
+                    _cachedPlayers.Add(currentPlayer);
+                return currentPlayer;
             }
         }
     }
